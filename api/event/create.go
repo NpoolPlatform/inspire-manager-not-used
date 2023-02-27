07d7573c2e91dd557e03bcdca1e7a2cd8950934d
Zhao KK <kikakkz@hotmail.com>
2023-02-27 11:53:47 +0000

    Could not be special offer

--- api/event/create.go
+++ api/event/create.go
@@ -66,7 +66,6 @@ func ValidateCreate(in *npool.EventReq) error { //nolint
 		switch coupon.GetCouponType() {
 		case alloccoupmgrpb.CouponType_FixAmount:
 		case alloccoupmgrpb.CouponType_Discount:
-		case alloccoupmgrpb.CouponType_SpecialOffer:
 		case alloccoupmgrpb.CouponType_ThresholdFixAmount:
 		case alloccoupmgrpb.CouponType_ThresholdDiscount:
 		case alloccoupmgrpb.CouponType_GoodFixAmount:
