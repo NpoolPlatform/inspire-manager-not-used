a81d69c6c8ca2f2331a2a66e9878430259d3bd86
Zhao KK <kikakkz@hotmail.com>
2023-03-01 12:08:54 +0000

    Return nil for only

---
+++ pkg/crud/event/crud.go
@@ -0,0 +1,475 @@
+package event
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	entevent "github.com/NpoolPlatform/inspire-manager/pkg/db/ent/event"
+	tracer "github.com/NpoolPlatform/inspire-manager/pkg/tracer/event"
+
+	constant "github.com/NpoolPlatform/inspire-manager/pkg/message/const"
+	commontracer "github.com/NpoolPlatform/inspire-manager/pkg/tracer"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/codes"
+
+	"github.com/NpoolPlatform/inspire-manager/pkg/db"
+	"github.com/NpoolPlatform/inspire-manager/pkg/db/ent"
+	"github.com/NpoolPlatform/libent-cruder/pkg/cruder"
+
+	basetypes "github.com/NpoolPlatform/message/npool/basetypes/v1"
+	npool "github.com/NpoolPlatform/message/npool/inspire/mgr/v1/event"
+
+	"github.com/google/uuid"
+	"github.com/shopspring/decimal"
+)
+
+func CreateSet(c *ent.EventCreate, in *npool.EventReq) (*ent.EventCreate, error) {
+	if in.ID != nil {
+		c.SetID(uuid.MustParse(in.GetID()))
+	}
+	if in.AppID != nil {
+		c.SetAppID(uuid.MustParse(in.GetAppID()))
+	}
+	if in.EventType != nil {
+		c.SetEventType(in.GetEventType().String())
+	}
+	if len(in.GetCoupons()) > 0 {
+		coupons := []npool.Coupon{}
+		for _, coup := range in.GetCoupons() {
+			coupons = append(coupons, npool.Coupon{
+				ID:         coup.ID,
+				CouponType: coup.CouponType,
+			})
+		}
+		c.SetCoupons(coupons)
+	}
+	if in.Credits != nil {
+		c.SetCredits(decimal.RequireFromString(in.GetCredits()))
+	}
+	if in.CreditsPerUSD != nil {
+		c.SetCreditsPerUsd(decimal.RequireFromString(in.GetCreditsPerUSD()))
+	}
+	if in.MaxConsecutive != nil {
+		c.SetMaxConsecutive(in.GetMaxConsecutive())
+	}
+	if in.GoodID != nil {
+		c.SetGoodID(uuid.MustParse(in.GetGoodID()))
+	}
+	if in.InviterLayers != nil {
+		c.SetInviterLayers(in.GetInviterLayers())
+	}
+	return c, nil
+}
+
+func Create(ctx context.Context, in *npool.EventReq) (*ent.Event, error) {
+	var info *ent.Event
+	var err error
+
+	_, span := otel.Tracer(constant.ServiceName).Start(ctx, "Create")
+	defer span.End()
+
+	defer func() {
+		if err != nil {
+			span.SetStatus(codes.Error, "db operation fail")
+			span.RecordError(err)
+		}
+	}()
+
+	span = tracer.Trace(span, in)
+
+	err = db.WithClient(ctx, func(_ctx context.Context, cli *ent.Client) error {
+		c := cli.Event.Create()
+		stm, err := CreateSet(c, in)
+		if err != nil {
+			return err
+		}
+		info, err = stm.Save(_ctx)
+		return err
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return info, nil
+}
+
+func CreateBulk(ctx context.Context, in []*npool.EventReq) ([]*ent.Event, error) {
+	var err error
+
+	_, span := otel.Tracer(constant.ServiceName).Start(ctx, "CreateBulk")
+	defer span.End()
+
+	defer func() {
+		if err != nil {
+			span.SetStatus(codes.Error, "db operation fail")
+			span.RecordError(err)
+		}
+	}()
+
+	span = tracer.TraceMany(span, in)
+
+	rows := []*ent.Event{}
+	err = db.WithTx(ctx, func(_ctx context.Context, tx *ent.Tx) error {
+		bulk := make([]*ent.EventCreate, len(in))
+		for i, info := range in {
+			bulk[i] = tx.Event.Create()
+			bulk[i], err = CreateSet(bulk[i], info)
+			if err != nil {
+				return err
+			}
+		}
+		rows, err = tx.Event.CreateBulk(bulk...).Save(_ctx)
+		return err
+	})
+	if err != nil {
+		return nil, err
+	}
+	return rows, nil
+}
+
+func UpdateSet(info *ent.Event, in *npool.EventReq) (*ent.EventUpdateOne, error) {
+	u := info.Update()
+
+	if in.Coupons != nil {
+		coupons := []npool.Coupon{}
+		for _, coup := range in.GetCoupons() {
+			coupons = append(coupons, npool.Coupon{
+				ID:         coup.ID,
+				CouponType: coup.CouponType,
+			})
+		}
+		u.SetCoupons(coupons)
+	}
+	if in.Credits != nil {
+		u.SetCredits(decimal.RequireFromString(in.GetCredits()))
+	}
+	if in.CreditsPerUSD != nil {
+		u.SetCreditsPerUsd(decimal.RequireFromString(in.GetCreditsPerUSD()))
+	}
+	if in.MaxConsecutive != nil {
+		u.SetMaxConsecutive(in.GetMaxConsecutive())
+	}
+	if in.InviterLayers != nil {
+		u.SetInviterLayers(in.GetInviterLayers())
+	}
+
+	return u, nil
+}
+
+func Update(ctx context.Context, in *npool.EventReq) (*ent.Event, error) {
+	var info *ent.Event
+	var err error
+
+	_, span := otel.Tracer(constant.ServiceName).Start(ctx, "Update")
+	defer span.End()
+
+	defer func() {
+		if err != nil {
+			span.SetStatus(codes.Error, "db operation fail")
+			span.RecordError(err)
+		}
+	}()
+
+	span = tracer.Trace(span, in)
+
+	err = db.WithTx(ctx, func(_ctx context.Context, tx *ent.Tx) error {
+		info, err = tx.Event.Query().Where(entevent.ID(uuid.MustParse(in.GetID()))).ForUpdate().Only(_ctx)
+		if err != nil {
+			return err
+		}
+
+		stm, err := UpdateSet(info, in)
+		if err != nil {
+			return err
+		}
+
+		info, err = stm.Save(_ctx)
+		return err
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return info, nil
+}
+
+func Row(ctx context.Context, id uuid.UUID) (*ent.Event, error) {
+	var info *ent.Event
+	var err error
+
+	_, span := otel.Tracer(constant.ServiceName).Start(ctx, "Row")
+	defer span.End()
+
+	defer func() {
+		if err != nil {
+			span.SetStatus(codes.Error, "db operation fail")
+			span.RecordError(err)
+		}
+	}()
+
+	span = commontracer.TraceID(span, id.String())
+
+	err = db.WithClient(ctx, func(_ctx context.Context, cli *ent.Client) error {
+		info, err = cli.Event.Query().Where(entevent.ID(id)).Only(_ctx)
+		return err
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return info, nil
+}
+
+func SetQueryConds(conds *npool.Conds, cli *ent.Client) (*ent.EventQuery, error) {
+	stm := cli.Event.Query()
+	if conds == nil {
+		return stm, nil
+	}
+	if conds.ID != nil {
+		switch conds.GetID().GetOp() {
+		case cruder.EQ:
+			stm.Where(entevent.ID(uuid.MustParse(conds.GetID().GetValue())))
+		default:
+			return nil, fmt.Errorf("invalid event field")
+		}
+	}
+	if conds.AppID != nil {
+		switch conds.GetAppID().GetOp() {
+		case cruder.EQ:
+			stm.Where(entevent.AppID(uuid.MustParse(conds.GetAppID().GetValue())))
+		default:
+			return nil, fmt.Errorf("invalid event field")
+		}
+	}
+	if conds.EventType != nil {
+		switch conds.GetEventType().GetOp() {
+		case cruder.EQ:
+			stm.Where(entevent.EventType(basetypes.UsedFor(conds.GetEventType().GetValue()).String()))
+		default:
+			return nil, fmt.Errorf("invalid event field")
+		}
+	}
+	if conds.GoodID != nil {
+		switch conds.GetGoodID().GetOp() {
+		case cruder.EQ:
+			stm.Where(entevent.GoodID(uuid.MustParse(conds.GetGoodID().GetValue())))
+		default:
+			return nil, fmt.Errorf("invalid event field")
+		}
+	}
+	return stm, nil
+}
+
+func Rows(ctx context.Context, conds *npool.Conds, offset, limit int) ([]*ent.Event, int, error) {
+	var err error
+
+	_, span := otel.Tracer(constant.ServiceName).Start(ctx, "Rows")
+	defer span.End()
+
+	defer func() {
+		if err != nil {
+			span.SetStatus(codes.Error, "db operation fail")
+			span.RecordError(err)
+		}
+	}()
+
+	span = tracer.TraceConds(span, conds)
+	span = commontracer.TraceOffsetLimit(span, offset, limit)
+
+	rows := []*ent.Event{}
+	var total int
+	err = db.WithClient(ctx, func(_ctx context.Context, cli *ent.Client) error {
+		stm, err := SetQueryConds(conds, cli)
+		if err != nil {
+			return err
+		}
+
+		total, err = stm.Count(_ctx)
+		if err != nil {
+			return err
+		}
+
+		rows, err = stm.
+			Offset(offset).
+			Order(ent.Desc(entevent.FieldUpdatedAt)).
+			Limit(limit).
+			All(_ctx)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+	if err != nil {
+		return nil, 0, err
+	}
+	return rows, total, nil
+}
+
+func RowOnly(ctx context.Context, conds *npool.Conds) (*ent.Event, error) {
+	var info *ent.Event
+	var err error
+
+	_, span := otel.Tracer(constant.ServiceName).Start(ctx, "RowOnly")
+	defer span.End()
+
+	defer func() {
+		if err != nil {
+			span.SetStatus(codes.Error, "db operation fail")
+			span.RecordError(err)
+		}
+	}()
+
+	span = tracer.TraceConds(span, conds)
+
+	err = db.WithClient(ctx, func(_ctx context.Context, cli *ent.Client) error {
+		stm, err := SetQueryConds(conds, cli)
+		if err != nil {
+			return err
+		}
+
+		info, err = stm.Only(_ctx)
+		if err != nil {
+			if ent.IsNotFound(err) {
+				return nil
+			}
+			return err
+		}
+
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return info, nil
+}
+
+func Count(ctx context.Context, conds *npool.Conds) (uint32, error) {
+	var err error
+	var total int
+
+	_, span := otel.Tracer(constant.ServiceName).Start(ctx, "Count")
+	defer span.End()
+
+	defer func() {
+		if err != nil {
+			span.SetStatus(codes.Error, "db operation fail")
+			span.RecordError(err)
+		}
+	}()
+
+	span = tracer.TraceConds(span, conds)
+
+	err = db.WithClient(ctx, func(_ctx context.Context, cli *ent.Client) error {
+		stm, err := SetQueryConds(conds, cli)
+		if err != nil {
+			return err
+		}
+
+		total, err = stm.Count(_ctx)
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+	if err != nil {
+		return 0, err
+	}
+
+	return uint32(total), nil
+}
+
+func Exist(ctx context.Context, id uuid.UUID) (bool, error) {
+	var err error
+	exist := false
+
+	_, span := otel.Tracer(constant.ServiceName).Start(ctx, "Exist")
+	defer span.End()
+
+	defer func() {
+		if err != nil {
+			span.SetStatus(codes.Error, "db operation fail")
+			span.RecordError(err)
+		}
+	}()
+
+	span = commontracer.TraceID(span, id.String())
+
+	err = db.WithClient(ctx, func(_ctx context.Context, cli *ent.Client) error {
+		exist, err = cli.Event.Query().Where(entevent.ID(id)).Exist(_ctx)
+		return err
+	})
+	if err != nil {
+		return false, err
+	}
+
+	return exist, nil
+}
+
+func ExistConds(ctx context.Context, conds *npool.Conds) (bool, error) {
+	var err error
+	exist := false
+
+	_, span := otel.Tracer(constant.ServiceName).Start(ctx, "ExistConds")
+	defer span.End()
+
+	defer func() {
+		if err != nil {
+			span.SetStatus(codes.Error, "db operation fail")
+			span.RecordError(err)
+		}
+	}()
+
+	span = tracer.TraceConds(span, conds)
+
+	err = db.WithClient(ctx, func(_ctx context.Context, cli *ent.Client) error {
+		stm, err := SetQueryConds(conds, cli)
+		if err != nil {
+			return err
+		}
+
+		exist, err = stm.Exist(_ctx)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+	if err != nil {
+		return false, err
+	}
+
+	return exist, nil
+}
+
+func Delete(ctx context.Context, id string) (*ent.Event, error) {
+	var info *ent.Event
+	var err error
+
+	_, span := otel.Tracer(constant.ServiceName).Start(ctx, "Delete")
+	defer span.End()
+
+	defer func() {
+		if err != nil {
+			span.SetStatus(codes.Error, "db operation fail")
+			span.RecordError(err)
+		}
+	}()
+
+	span = commontracer.TraceID(span, id)
+
+	err = db.WithClient(ctx, func(_ctx context.Context, cli *ent.Client) error {
+		info, err = cli.Event.UpdateOneID(uuid.MustParse(id)).
+			SetDeletedAt(uint32(time.Now().Unix())).
+			Save(_ctx)
+		return err
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return info, nil
+}
